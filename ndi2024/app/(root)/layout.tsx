79b2dc96043037cf9a240ad4e61891d4d805f593
Erwan <erwan.coubret@gmail.com>
2024-12-05 15:19:09 +0100

    C partiiiiiii

---
+++ ndi2024/app/(root)/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Fugaz_One, Rubik } from 'next/font/google'
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "NDI 2024",
+  description:
+    "Nuit de l'info 2024",
+};
+
+const fugazOne = Fugaz_One({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-fugazOne",
+});
+
+const rubik = Rubik({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-rubik",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${fugazOne.variable} ${rubik.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
