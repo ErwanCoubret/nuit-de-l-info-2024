3ab91796620071749cbaa83ce8633b6da178eb66
Erwan <erwan.coubret@gmail.com>
2024-12-05 16:00:15 +0100

    hop

--- ndi2024/app/(root)/layout.tsx
+++ ndi2024/app/(root)/layout.tsx
@@ -1,36 +1,34 @@
 import type { Metadata } from "next";
-import { Fugaz_One, Rubik } from 'next/font/google'
+import { Fugaz_One, Rubik } from "next/font/google";
 import "../globals.css";
 
 export const metadata: Metadata = {
-  title: "NDI 2024",
-  description:
-    "Nuit de l'info 2024",
+	title: "NDI 2024",
+	description: "Nuit de l'info 2024",
 };
 
 const fugazOne = Fugaz_One({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-fugazOne",
+	weight: "400",
+	subsets: ["latin"],
+	variable: "--font-fugazOne",
 });
 
 const rubik = Rubik({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-rubik",
+	weight: "400",
+	subsets: ["latin"],
+	variable: "--font-rubik",
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${fugazOne.variable} ${rubik.variable}`}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="fr">
+			<body className={`${fugazOne.variable} ${rubik.variable}`}>
+				{children}
+			</body>
+		</html>
+	);
 }
