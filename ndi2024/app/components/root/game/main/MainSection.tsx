b5cc90a0bdba3309cbcc04d017e02cd76f6a8d3c
Erwan <erwan.coubret@gmail.com>
2024-12-06 06:53:10 +0100

    allez

--- ndi2024/app/components/root/game/main/MainSection.tsx
+++ ndi2024/app/components/root/game/main/MainSection.tsx
@@ -33,7 +33,9 @@ function Man({
 			{/* Chest as the central element */}
 			<Link
 				href={chestFlag ? "" : "/game/minigames/chest"}
-				className={`absolute  z-10 ${chestFlag ? "cursor-not-allowed" : "cursor-pointer"}`}
+				className={`absolute  z-10 ${
+					chestFlag ? "cursor-not-allowed" : "cursor-pointer"
+				}`}
 			>
 				<div className="relative xl:w-[10vw] xl:h-[10vw] lg:w-[20vw] lg:h-[20vw] w-[22vw] h-[22vw]">
 					<Image
@@ -48,7 +50,9 @@ function Man({
 			{/* Head positioned above the chest */}
 			<Link
 				href={headFlag ? "" : "/game/minigames/head"}
-				className={`absolute z-20 ${headFlag ? "cursor-not-allowed" : "cursor-pointer"}`}
+				className={`absolute z-20 ${
+					headFlag ? "cursor-not-allowed" : "cursor-pointer"
+				}`}
 				style={{
 					transform: "translate(-50%, -145%)", // Position relative to chest
 					top: "50%", // Align vertically
@@ -68,7 +72,9 @@ function Man({
 			{/* Left Arm */}
 			<Link
 				href={armsFlag ? "" : "/game/minigames/arms"}
-				className={`absolute ${armsFlag ? "cursor-not-allowed" : "cursor-pointer"}`}
+				className={`absolute ${
+					armsFlag ? "cursor-not-allowed" : "cursor-pointer"
+				}`}
 				style={{
 					transform: "translate(-100%, -100%) rotate(-45deg)", // Position relative to chest
 					top: "50%",
@@ -88,7 +94,9 @@ function Man({
 			{/* Right Arm */}
 			<Link
 				href={armsFlag ? "" : "/game/minigames/arms"}
-				className={`absolute ${armsFlag ? "cursor-not-allowed" : "cursor-pointer"}`}
+				className={`absolute ${
+					armsFlag ? "cursor-not-allowed" : "cursor-pointer"
+				}`}
 				style={{
 					transform: "translate(0%, -100%) rotate(45deg)", // Position relative to chest
 					top: "50%",
@@ -108,7 +116,9 @@ function Man({
 			{/* Left Leg */}
 			<Link
 				href={legsFlag ? "" : "/game/minigames/legs"}
-				className={`absolute ${legsFlag ? "cursor-not-allowed" : "cursor-pointer"}`}
+				className={`absolute ${
+					legsFlag ? "cursor-not-allowed" : "cursor-pointer"
+				}`}
 				style={{
 					transform: "translate(-100%, 10%) rotate(45deg)", // Position relative to chest
 					top: "50%",
@@ -128,7 +138,9 @@ function Man({
 			{/* Right Leg */}
 			<Link
 				href={legsFlag ? "" : "/game/minigames/legs"}
-				className={`absolute ${legsFlag ? "cursor-not-allowed" : "cursor-pointer"}`}
+				className={`absolute ${
+					legsFlag ? "cursor-not-allowed" : "cursor-pointer"
+				}`}
 				style={{
 					transform: "translate(0%, 10%) rotate(-45deg)", // Position relative to chest
 					top: "50%",
@@ -154,34 +166,50 @@ export default function MainSection() {
 	const [chestFlag, setChestFlag] = useState(false);
 	const [legsFlag, setLegsFlag] = useState(false);
 
+	// Effect pour récupérer les flags depuis le localStorage
 	useEffect(() => {
-		window.localStorage.getItem("armsFlag") === "1" && setArmsFlag(true);
-		window.localStorage.getItem("headFlag") === "1" && setHeadFlag(true);
-		window.localStorage.getItem("chestFlag") === "1" && setChestFlag(true);
-		window.localStorage.getItem("legsFlag") === "1" && setLegsFlag(true);
+		const armsFlagFromStorage = window.localStorage.getItem("armsFlag");
+		const headFlagFromStorage = window.localStorage.getItem("headFlag");
+		const chestFlagFromStorage = window.localStorage.getItem("chestFlag");
+		const legsFlagFromStorage = window.localStorage.getItem("legsFlag");
 
-    if (armsFlag && headFlag && chestFlag && legsFlag) {
-      window.location.href = "/game/ending";
-    }
+		if (armsFlagFromStorage === "1") setArmsFlag(true);
+		if (headFlagFromStorage === "1") setHeadFlag(true);
+		if (chestFlagFromStorage === "1") setChestFlag(true);
+		if (legsFlagFromStorage === "1") setLegsFlag(true);
 	}, []);
+
+	// Effect pour vérifier si toutes les parties sont guéries
+	useEffect(() => {
+		if (armsFlag && headFlag && chestFlag && legsFlag) {
+			window.location.href = "/game/ending";
+		}
+	}, [armsFlag, headFlag, chestFlag, legsFlag]);
+
+	const remainingParts =
+		4 -
+		(armsFlag ? 1 : 0) -
+		(headFlag ? 1 : 0) -
+		(chestFlag ? 1 : 0) -
+		(legsFlag ? 1 : 0);
 
 	return (
 		<div className="min-h-screen w-full flex flex-col items-center justify-center bg-primary-800 text-white">
+			{/* Titre */}
 			<div className="absolute top-20 flex flex-col justify-center lg:mb-0 mx-auto text-center p-5">
 				<h1 className="text-2xl lg:text-4xl font-black text-primary-400 text-nowrap opacity-0 animate-fade">
 					SAUVEZ LE NAUFRAGÉ
 				</h1>
-
 				<p className="text-white text-lg lg:text-xl opacity-0 animate-fade delay-200">
 					Cliquez sur une{" "}
 					<span className="mx-1 font-bold text-red-500">
-						{" "}
-						partie malade{" "}
+						partie malade
 					</span>{" "}
 					du corps du naufragé pour lancer un mini-jeu
 				</p>
 			</div>
 
+			{/* Corps du personnage */}
 			<Man
 				armsFlag={armsFlag}
 				headFlag={headFlag}
@@ -189,17 +217,13 @@ export default function MainSection() {
 				legsFlag={legsFlag}
 			/>
 
+			{/* Nombre de parties restantes */}
 			<div className="absolute bottom-20 flex flex-col justify-center lg:mb-0 mx-auto text-center p-5">
 				<p className="text-white text-lg lg:text-xl opacity-0 animate-fade delay-600 italic">
-					Encore
+					Encore{" "}
 					<span className="mx-1 font-bold text-red-500">
-						{4 -
-							(armsFlag ? 1 : 0) -
-							(headFlag ? 1 : 0) -
-							(chestFlag ? 1 : 0) -
-							(legsFlag ? 1 : 0)}{" "}
-						parties
-					</span>
+						{remainingParts} parties
+					</span>{" "}
 					malades à soigner
 				</p>
 			</div>
